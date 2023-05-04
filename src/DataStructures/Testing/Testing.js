40e93ad8d0a2c8d7a4b852463153c35a09e6acb8
sagarhasan273 <sagarhasan273@gmail.com>
2023-05-04 12:11:22 +0600

    create line testing

--- src/DataStructures/Testing/Testing.js
+++ src/DataStructures/Testing/Testing.js
@@ -2,13 +2,16 @@ import './Testing.css';
 
 export default function Testing() {
   return (
-    <div className="containerT">
-      <div className="node node1" />
-      <div className="node node2" />
-      <div className="node node3" />
-      <div className="edge edge1" />
-      <div className="edge edge2" />
-    </div>
+    <main>
+      <div className="node" style={{ top: '150px', left: '50px' }} />
+      <div className="node" style={{ top: '150px', left: '450px' }} />
+      startX startY c width-stiffness 0, stiffness height, endX endY
+      <svg>
+        <path d="M60 160, 435 160" strokeWidth={4} stroke="black" fill="transparent" />
+      </svg>
+      <image className="imagetesting" href="./sendmessage.png" height="20px" width="20px" style={{ top: '50px', left: '430px' }} />
+      <div>hellow</div>
+    </main>
 
   );
 }
